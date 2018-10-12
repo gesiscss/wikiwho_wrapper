6e72c8a105f520332edb952695fc481f5c0671a9
Ulloa, Roberto <roberto.ulloa@gesis.org>
2018-10-12 16:45:31 +0200

    completing test

--- tests/test_wikiwhowrapper.py
+++ tests/test_wikiwhowrapper.py
@@ -1,5 +1,14 @@
 
+from pytest import fixture
+
 from wikiwho_wrapper import WikiWhoAPI
+
+@fixture
+def all_content_keys():
+    # Responsible only for returning the test data
+    return ['id', 'origin_country', 'poster_path', 'name',
+              'overview', 'popularity', 'backdrop_path',
+              'first_air_date', 'vote_count', 'vote_average']
 
 def test_wikiwho_all_content():
     """Tests an API call to get a TV show's info"""
@@ -9,3 +18,4 @@ def test_wikiwho_all_content():
 
     assert isinstance(response, dict)
     assert response['page_id'] == 6187, "The ID should be in the response"
+    assert set(all_content_keys()).issubset(response.keys()), "All keys should be in the response"
