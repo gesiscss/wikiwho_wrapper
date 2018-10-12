5d74113ef12a72d91821eee167f05b78820ccc76
Ulloa, Roberto <roberto.ulloa@gesis.org>
2018-10-12 18:27:08 +0200

    all_content request is completed and documented

--- wikiwho_wrapper/__init__.py
+++ wikiwho_wrapper/__init__.py
@@ -1,1 +1,19 @@
+import os
+import requests
+
+WIKIWHO_API_KEY = os.environ.get('WIKIWHO_API_KEY', None)
+
+class APIKeyMissingError(Exception):
+    pass
+
+# if WIKIWHO_API_KEY is None:
+#     raise APIKeyMissingError(
+#         "All methods require an API key."
+#     )
+
+session = requests.Session()
+#session.params = {}
+#session.params['api_key'] = WIKIWHO_API_KEY
+
+
 from .wikiwhoapi import WikiWhoAPI
