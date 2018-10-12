351baa31fc4eaa7d910c57e6d690aec034502798
Ulloa, Roberto <roberto.ur@gmail.com>
2018-10-12 18:27:08 +0200

    all_content request is completed and documented

--- tests/test_wikiwhowrapper.py
+++ tests/test_wikiwhowrapper.py
@@ -7,25 +7,28 @@ from wikiwho_wrapper import WikiWhoAPI
 @fixture
 def all_content_keys():
     # Responsible only for returning the test data
-    return [ "message", "success", "page_id", "article_title", "all_tokens", ]
-
-def test_wikiwho_all_content_by_id():
-    """Tests an API call to get a all content of a page"""
-
-    api = WikiWhoAPI(6187)
-    response = api.all_content()
-
-    assert isinstance(response, dict)
-    assert response['page_id'] == 6187, "The ID should be in the response"
-    assert set(all_content_keys()).issubset(response.keys()), "All keys should be in the response"
+    return ["message", "success", "page_id", "article_title", "all_tokens"]
 
 
-def test_wikiwho_all_content_by_name():
-    """Tests an API call to get a all content of a page"""
+def test_wikiwho_all_content_by_id(all_content_keys):
+    """Tests an API call to get a all content of a page by id"""
 
-    api = WikiWhoAPI("Cologne")
-    response = api.all_content()
+    api = WikiWhoAPI()
+    response = api.all_content(6187)
 
     assert isinstance(response, dict)
     assert response['page_id'] == 6187, "The ID should be in the response"
-    assert set(all_content_keys()).issubset(response.keys()), "All keys should be in the response"
+    assert set(all_content_keys).issubset(
+        response.keys()), "All keys should be in the response"
+
+
+def test_wikiwho_all_content_by_title(all_content_keys):
+    """Tests an API call to get a all content of a page by title"""
+
+    api = WikiWhoAPI()
+    response = api.all_content("Cologne")
+
+    assert isinstance(response, dict)
+    assert response['page_id'] == 6187, "The ID should be in the response"
+    assert set(all_content_keys).issubset(
+        response.keys()), "All keys should be in the response"
