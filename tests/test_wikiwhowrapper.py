6daeaddd38a9ae06d2388dfcecc7ef5a678488b6
Ulloa, Roberto <roberto.ulloa@gesis.org>
2018-10-12 17:05:29 +0200

    new test of wikiwho all content by test

--- tests/test_wikiwhowrapper.py
+++ tests/test_wikiwhowrapper.py
@@ -3,17 +3,27 @@ from pytest import fixture
 
 from wikiwho_wrapper import WikiWhoAPI
 
+
 @fixture
 def all_content_keys():
     # Responsible only for returning the test data
-    return ['id', 'origin_country', 'poster_path', 'name',
-              'overview', 'popularity', 'backdrop_path',
-              'first_air_date', 'vote_count', 'vote_average']
+    return [ "message", "success", "page_id", "article_title", "all_tokens", ]
 
-def test_wikiwho_all_content():
-    """Tests an API call to get a TV show's info"""
+def test_wikiwho_all_content_by_id():
+    """Tests an API call to get a all content of a page"""
 
     api = WikiWhoAPI(6187)
+    response = api.all_content()
+
+    assert isinstance(response, dict)
+    assert response['page_id'] == 6187, "The ID should be in the response"
+    assert set(all_content_keys()).issubset(response.keys()), "All keys should be in the response"
+
+
+def test_wikiwho_all_content_by_name():
+    """Tests an API call to get a all content of a page"""
+
+    api = WikiWhoAPI("Cologne")
     response = api.all_content()
 
     assert isinstance(response, dict)
