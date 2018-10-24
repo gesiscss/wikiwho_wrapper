89a54388aa9ae3d1b00f7a269312c840d2280dee
AliAghel <a.aghelmaleki@gmail.com>
2018-10-24 17:11:15 +0200

    revision queries are ready

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ from wikiwho_wrapper import WikiWhoAPI
 api = WikiWhoAPI()
 
 # nice example: bioglass
-response = api.all_content("Cologne")
+# response = api.all_content("Cologne")
+response = api.range_rev_content_by_article_title("bioglass", 18064039, 79583319)
 import ipdb; ipdb.set_trace()  # breakpoint 12fb971f //
 
