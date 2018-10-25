8bec7e5d40c93f43de7b1570e460d27031564905
AliAghel <a.aghelmaleki@gmail.com>
2018-10-25 12:50:42 +0200

    complete all wrappers

--- tests/test_wikiwhowrapper.py
+++ tests/test_wikiwhowrapper.py
@@ -71,7 +71,8 @@ def test_wikiwho_specific_rev_content_by_id(rev_content_keys):
     response = api.specific_rev_content_by_rev_id(503680497)
 
     assert isinstance(response, dict)
-    assert '503680497' in response['revisions'][0], "The ID should be in the response"
+    assert '503680497' in response['revisions'][
+        0], "The ID should be in the response"
     # assert response['success'] == True, "success should be true in the response"
     assert set(rev_content_keys).issubset(
         response.keys()), "All keys should be in the response"
@@ -85,7 +86,8 @@ def test_wikiwho_specific_rev_content_by_title(rev_content_keys):
 
     assert isinstance(response, dict)
     assert response['page_id'] == 6187, "The ID should be in the response"
-    assert '503680497' in response['revisions'][0], "The ID should be in the response"
+    assert '503680497' in response['revisions'][
+        0], "The ID should be in the response"
     assert set(rev_content_keys).issubset(
         response.keys()), "All keys should be in the response"
 
@@ -94,12 +96,38 @@ def test_wikiwho_range_rev_content_by_article_title(rev_content_keys):
     """Tests an API call to get the content of revisions from start revision to end revision ordered by timestamp of the given article by title"""
 
     api = WikiWhoAPI()
-    response = api.range_rev_content_by_article_title("bioglass", 18064039, 79583319)
+    response = api.range_rev_content_by_article_title(
+        "bioglass", 18064039, 79583319)
 
     assert isinstance(response, dict)
     assert response['page_id'] == 2161298, "The ID should be in the response"
-    assert '18064039' in response['revisions'][0], "The ID should be in the response"
+    assert '18064039' in response['revisions'][
+        0], "The ID should be in the response"
     assert len(response['revisions']) == 10
     assert set(rev_content_keys).issubset(
         response.keys()), "All keys should be in the response"
 
+
+def test_wikiwho_article_rev_id_by_page_id(rev_content_keys):
+    """Tests an API call to get revision IDs of the given article as processed by WikiWho of the given article by id"""
+
+    api = WikiWhoAPI()
+    response = api.rev_ids_of_article(2161298)
+
+    assert isinstance(response, dict)
+    assert response['page_id'] == 2161298, "The ID should be in the response"
+    assert len(response['revisions']) == 175
+    assert set(rev_content_keys).issubset(
+        response.keys()), "All keys should be in the response"
+
+
+def test_wikiwho_article_rev_id_by_article_title(rev_content_keys):
+    """Tests an API call to get revision IDs of the given article as processed by WikiWho of the given article by title"""
+
+    api = WikiWhoAPI()
+    response = api.rev_ids_of_article("bioglass")
+
+    assert isinstance(response, dict)
+    assert response['page_id'] == 2161298, "The ID should be in the response"
+    assert set(rev_content_keys).issubset(
+        response.keys()), "All keys should be in the response"
