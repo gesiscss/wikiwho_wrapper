91611809410c26f2d170e3557f8c573b767e3795
Ali <aaghelmaleki@gesis.org>
2018-11-08 09:57:15 +0100

    Design quereis for return tables

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from wikiwho_wrapper import WikiWhoAPI, all_content
+from wikiwho_wrapper import WikiWhoAPI, all_content, last_rev_content, specific_rev_content_by_article_title, specific_rev_content_by_rev_id, range_rev_content_by_article_title, rev_ids_of_article
 
 api = WikiWhoAPI()
 
@@ -7,6 +7,22 @@ api = WikiWhoAPI()
 # response = api.range_rev_content_by_article_title("bioglass", 18064039, 79583319)
 # response = api.rev_ids_of_article(2161298)
 
-df = all_content()
-import ipdb; ipdb.set_trace()  # breakpoint 12fb971f //
+df0 = all_content(article="bioglass")
+df1 = all_content(2161298)
 
+df2 = last_rev_content(article="bioglass")
+df3 = last_rev_content(2161298)
+
+df4 = specific_rev_content_by_rev_id(rev_id=189370281)
+
+df5 = specific_rev_content_by_article_title(article_title="bioglass", rev_id=189370281)
+
+df6 = range_rev_content_by_article_title(article_title="bioglass",  start_rev=18064039,  end_rev=207995408)
+
+df7 = rev_ids_of_article(article="bioglass")
+df8 = rev_ids_of_article(2161298)
+
+
+
+import ipdb
+ipdb.set_trace()
