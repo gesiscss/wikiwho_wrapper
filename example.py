7e03d56997609a50cf02a7915ef788c9b465d470
AliAghel <a.aghelmaleki@gmail.com>
2018-11-08 12:46:43 +0100

    rename datafarames in example.py

--- example.py
+++ example.py
@@ -9,19 +9,19 @@ api = WikiWhoAPI()
 querier = APIQuerier(api)
 
 
+all_content_by_given_article_title_df = querier.all_content(article="bioglass")
+all_content_by_given_page_id_df = querier.all_content(2161298)
 
-df0 = querier.all_content(article="bioglass")
-df1 = querier.all_content(2161298)
+last_revision_by_given_article_title_df = querier.last_rev_content(article="bioglass")
+last_revision_by_given_page_id_df = querier.last_rev_content(2161298)
 
-df2 = querier.last_rev_content(article="bioglass")
-df3 = querier.last_rev_content(2161298)
-df4 = querier.specific_rev_content_by_rev_id(rev_id=189370281)
-df5 = querier.specific_rev_content_by_article_title(article_title="bioglass", rev_id=189370281)
-df6 = querier.range_rev_content_by_article_title(article_title="bioglass",  start_rev=18064039,  end_rev=207995408)
+specific_revision_by_given_article_title_df = querier.specific_rev_content_by_rev_id(rev_id=189370281)
+specific_revision_by_given_revision_id_df = querier.specific_rev_content_by_article_title(article_title="bioglass", rev_id=189370281)
 
-df7 = querier.rev_ids_of_article(article="bioglass")
-df8 = querier.rev_ids_of_article(2161298)
+range_revisions_by_given_start_to_end_revision_ids_df = querier.range_rev_content_by_article_title(article_title="bioglass",  start_rev=18064039,  end_rev=207995408)
 
+revision_ids_of_article_by_given_article_title_df = querier.rev_ids_of_article(article="bioglass")
+revision_ids_of_article_by_given_page_id_df = querier.rev_ids_of_article(2161298)
 
 
 import ipdb
