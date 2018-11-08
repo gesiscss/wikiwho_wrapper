b0efd67cef5de47f393e8ef9d7c39df9c0f38707
AliAghel <a.aghelmaleki@gmail.com>
2018-11-08 11:18:51 +0100

    converting queries into class APIQuerier

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from wikiwho_wrapper import WikiWhoAPI, all_content, last_rev_content, specific_rev_content_by_article_title, specific_rev_content_by_rev_id, range_rev_content_by_article_title, rev_ids_of_article
+from wikiwho_wrapper import WikiWhoAPI, APIQuerier
 
 api = WikiWhoAPI()
 
@@ -6,21 +6,21 @@ api = WikiWhoAPI()
 # response = api.all_content("Cologne")
 # response = api.range_rev_content_by_article_title("bioglass", 18064039, 79583319)
 # response = api.rev_ids_of_article(2161298)
+querier = APIQuerier(api)
 
-df0 = all_content(article="bioglass")
-df1 = all_content(2161298)
 
-df2 = last_rev_content(article="bioglass")
-df3 = last_rev_content(2161298)
 
-df4 = specific_rev_content_by_rev_id(rev_id=189370281)
+df0 = querier.all_content(article="bioglass")
+df1 = querier.all_content(2161298)
 
-df5 = specific_rev_content_by_article_title(article_title="bioglass", rev_id=189370281)
+df2 = querier.last_rev_content(article="bioglass")
+df3 = querier.last_rev_content(2161298)
+df4 = querier.specific_rev_content_by_rev_id(rev_id=189370281)
+df5 = querier.specific_rev_content_by_article_title(article_title="bioglass", rev_id=189370281)
+df6 = querier.range_rev_content_by_article_title(article_title="bioglass",  start_rev=18064039,  end_rev=207995408)
 
-df6 = range_rev_content_by_article_title(article_title="bioglass",  start_rev=18064039,  end_rev=207995408)
-
-df7 = rev_ids_of_article(article="bioglass")
-df8 = rev_ids_of_article(2161298)
+df7 = querier.rev_ids_of_article(article="bioglass")
+df8 = querier.rev_ids_of_article(2161298)
 
 
 
